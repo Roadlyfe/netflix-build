803d541cd7748508854b3457eaad4c9383dee3d8
Adam Smith <adamsmith@Adams-MacBook-Air.local>
2022-11-01 11:58:29 -0700

    initiated reactrouter5 and login components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-     <HomeScreen /> 
+
+     <Router>
+       <Switch>
+          <Route exact path="/" >
+            <HomeScreen /> 
+         </Route>
+        </Switch>
+    </Router>
     </div>
   );
 }
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './LoginScreen.css'
+
+function LoginScreen() {
+  return (
+    <div className='loginScreen'>
+
+    </div>
+  )
+}
+
+export default LoginScreen
